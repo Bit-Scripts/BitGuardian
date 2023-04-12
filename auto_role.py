cfed9d1feb604be0b154f2496e06822b674223c7
Quentin-D31 <QuentinDelatorre@tutanota.com>
2023-04-12 23:36:17 +0200

    Modification de on_member_join pour utiliser l'ID du rôle au lieu du nom

--- auto_role.py
+++ auto_role.py
@@ -1,12 +1,13 @@
 import discord
 
 async def on_member_join(member):
+    role_id = 1095447703749677269 
     try:
-        role = discord.utils.get(member.guild.roles, name='Utilisateur')
+        role = discord.utils.get(member.guild.roles, id=role_id)
         if role is None:
-            print("Le rôle 'Utilisateur' n'a pas été trouvé. Veuillez créer ce rôle sur votre serveur.")
+            print(f"Le rôle avec l'ID {role_id} n'a pas été trouvé. Veuillez vérifier que l'ID est correct.")
             return
         await member.add_roles(role)
-        print(f"Le rôle 'Utilisateur' a été attribué à {member}.")
+        print(f"Le rôle {role.name} a été attribué à {member}.")
     except Exception as error:
         print(f"Erreur lors de l'attribution du rôle : {error}")
